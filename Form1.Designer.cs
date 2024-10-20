e0818bd55477597717d1b7b5f200f124a7a9760d
siteknower <office@siteknower.com>
2024-10-20 15:10:55 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace StnwSampleCS04
+{
+    partial class frmInvoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCustomer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSaveSchema = new System.Windows.Forms.Button();
+            this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Code,
+            this.Nam,
+            this.Amount,
+            this.Price});
+            this.DataGridView1.Location = new System.Drawing.Point(22, 71);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(444, 133);
+            this.DataGridView1.TabIndex = 83;
+            // 
+            // txtCustomer
+            // 
+            this.txtCustomer.Location = new System.Drawing.Point(22, 33);
+            this.txtCustomer.Name = "txtCustomer";
+            this.txtCustomer.Size = new System.Drawing.Size(352, 20);
+            this.txtCustomer.TabIndex = 84;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 85;
+            this.label1.Text = "Customer:";
+            // 
+            // btnSaveSchema
+            // 
+            this.btnSaveSchema.Location = new System.Drawing.Point(25, 242);
+            this.btnSaveSchema.Name = "btnSaveSchema";
+            this.btnSaveSchema.Size = new System.Drawing.Size(110, 23);
+            this.btnSaveSchema.TabIndex = 86;
+            this.btnSaveSchema.Text = "Save schema";
+            this.btnSaveSchema.UseVisualStyleBackColor = true;
+            this.btnSaveSchema.Click += new System.EventHandler(this.btnSaveSchema_Click);
+            // 
+            // Code
+            // 
+            this.Code.DataPropertyName = "Code";
+            this.Code.HeaderText = "Code";
+            this.Code.Name = "Code";
+            // 
+            // Nam
+            // 
+            this.Nam.DataPropertyName = "Name";
+            this.Nam.HeaderText = "Name";
+            this.Nam.Name = "Nam";
+            // 
+            // Amount
+            // 
+            this.Amount.DataPropertyName = "Amount";
+            this.Amount.HeaderText = "Amount";
+            this.Amount.Name = "Amount";
+            this.Amount.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Price
+            // 
+            this.Price.DataPropertyName = "Price";
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            this.Price.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(391, 242);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 87;
+            this.Button1.Text = "Print";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // frmInvoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 306);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.btnSaveSchema);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCustomer);
+            this.Controls.Add(this.DataGridView1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmInvoice";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Invoice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.TextBox txtCustomer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSaveSchema;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        internal System.Windows.Forms.Button Button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using StnwService;
+
+namespace StnwSampleCS04
+{
+    public partial class frmInvoice : Form
+    {
+        DataSet dsInvoice = new DataSet();
+        public frmInvoice()
+        {
+            InitializeComponent();
+
+            this.DataGridView1.AutoGenerateColumns = false;
+
+            dsInvoice.Tables.Add(new DataTable("InvItems"));
+            DataTable dt = dsInvoice.Tables["InvItems"];
+            dt.Columns.Add("Id", typeof(string));
+            dt.Columns.Add("Code", typeof(string));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Amount", typeof(double));
+            dt.Columns.Add("Price", typeof(double));
+
+            dsInvoice.Tables.Add(new DataTable("Dummy"));
+            DataTable dtd = dsInvoice.Tables["Dummy"];
+            dtd.Columns.Add("Text1", typeof(string));
+
+            DataSet tds = GetData();
+
+            txtCustomer.DataBindings.Add("Text", dtd, "Text1");
+
+            DataGridView1.DataSource = tds.Tables["InvItems"];
+
+            this.DataGridView1.Columns["Amount"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            this.DataGridView1.Columns["Price"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            this.DataGridView1.Columns["Amount"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+            this.DataGridView1.Columns["Price"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            foreach (DataGridViewColumn column in DataGridView1.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            this.DataGridView1.CellValidating += DataGridView1_CellValidating;
+
+        }
+
+        private DataSet GetData()
+        {
+            DataTable dt = dsInvoice.Tables["InvItems"];
+           
+            DataRow dr;
+            dr = dt.NewRow();
+            dr["Id"] = Guid.NewGuid().ToString();
+            dr["Code"] = "BB8527";
+            dr["Name"] = "Bread";
+            dr["Amount"] = 1;
+            dr["Price"] = 2.15;
+            dsInvoice.Tables["InvItems"].Rows.Add(dr);
+            dr.EndEdit();
+            dr = dt.NewRow();
+            dr["Id"] = Guid.NewGuid().ToString();
+            dr["Code"] ="SA482";
+            dr["Name"] = "Chocolate";
+            dr["Amount"] = 2;
+            dr["Price"] = 7.65;
+            dsInvoice.Tables["InvItems"].Rows.Add(dr);
+            dr.EndEdit();
+            dr = dt.NewRow();
+            dr["Id"] = Guid.NewGuid().ToString();
+            dr["Code"] = "QCI24";
+            dr["Name"] = "Cheese";
+            dr["Amount"] = 1;
+            dr["Price"] = 2.08;
+            dsInvoice.Tables["InvItems"].Rows.Add(dr);
+            dr.EndEdit();
+            dr = dt.NewRow();
+            dr["Id"] = Guid.NewGuid().ToString();
+            dr["Code"] = "MOX58";
+            dr["Name"] = "Juice";
+            dr["Amount"] = 1;
+            dr["Price"] = 3.55;
+            dsInvoice.Tables["InvItems"].Rows.Add(dr);
+            dr.EndEdit();
+            dr = dt.NewRow();
+            dr["Id"] = Guid.NewGuid().ToString();
+            dr["Code"] = "PB154";
+            dr["Name"] = "Milk";
+            dr["Amount"] = 3;
+            dr["Price"] = 7.87;
+            dsInvoice.Tables["InvItems"].Rows.Add(dr);
+            dr.EndEdit();
+
+            DataTable dtd = dsInvoice.Tables["Dummy"];
+            dr = dtd.NewRow();
+            dr["Text1"] = "Phoenixer inc.";
+            dsInvoice.Tables["Dummy"].Rows.Add(dr);
+            dr.EndEdit();
+
+            return dsInvoice;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaveSchema_Click(object sender, EventArgs e)
+        {
+            string schemaXml = "";
+            using (StringWriter sw = new StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(sw))
+                {
+                    dsInvoice.WriteXmlSchema(xmlWriter);
+                }
+                schemaXml = sw.ToString();
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.FileName = "schema.xml";
+                saveFileDialog1.Filter = "XML file|*.xml";
+                saveFileDialog1.Title = "Save the schema file";
+                saveFileDialog1.InitialDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {                   
+                    File.WriteAllText(saveFileDialog1.FileName, schemaXml);
+                }
+            }
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            clsStnwClass tsi = new clsStnwClass();
+            string preslAccountCode = "DEMO1";    // your account code
+            string preslUserCode = "0000";  // your user code
+
+            tsi.preslAccountCode = preslAccountCode;
+            tsi.preslUserCode = preslUserCode;
+            tsi.dsRPT = dsInvoice;
+            tsi.RPTDEST = 0;
+
+            string rptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "InvoiceReport.rpt");
+            tsi.ReportFullName = rptPath;
+
+            tsi.ShowForm();
+        }
+
+      
+        private void DataGridView1_CellValidating(object sender,
+                                          DataGridViewCellValidatingEventArgs e)
+        {
+            if ((e.ColumnIndex == 2) || (e.ColumnIndex == 3))
+            {
+                decimal dec;
+                string s = e.FormattedValue.ToString().Trim('0');
+
+                if (!decimal.TryParse(s, out dec))
+                {
+                    e.Cancel = true;
+                    MessageBox.Show("please enter numeric");
+                }
+            }
+        }
+
+    }
+}
